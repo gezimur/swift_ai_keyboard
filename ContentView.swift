6309f5ffc938408ff42d2368b3428611f6f1fe99
gezimur <gena.ziganshin@yandex.ru>
2026-01-14 22:06:35 +0300

    Try to fix keyboard states

--- ContentView.swift
+++ ContentView.swift
@@ -1,44 +1,48 @@
 import SwiftUI
 
-struct ContentView: View {
-    @State private var current_state: String = "keys + suggestions"
+struct StateParams {
+    var state: String
+    var args: [String]
+}
 
-    @State private var prev_state: [String] = ["keys + suggestions"]
-    @State private var current_args: [String] = [""]
+struct ContentView: View {
+    @State private var current_state: StateParams = StateParams(state: "keys + suggestions", args: [])
+    @State private var prev_state: [StateParams] = [StateParams(state: "keys + suggestions", args: [])]
 
     var body: some View {
-        if self.current_state == "keys + suggestions" {
+        if self.current_state.state == "keys + suggestions" {
             VStack{
                 SuggestionsPanel(request_subscriber: self.procRequest)
                 KeyboardPanel(request_subscriber: self.procRequest)
             }
-        } else if self.current_state == "keys + features" {
+        } else if self.current_state.state == "keys + features" {
             VStack{
                 MiniFeaturesPanel(request_subscriber: self.procRequest)
                 KeyboardPanel(request_subscriber: self.procRequest)
             }
-        } else if self.current_state == "features" {
+        } else if self.current_state.state == "features" {
             FeaturesPanel(request_subscriber: self.procRequest)
-        } else if self.current_state == "keys + input tool" {
+        } else if self.current_state.state == "keys + input tool" {
             VStack{
-                InputTool(feature_type: self.current_args[0], request_subscriber: self.procRequest)
+                InputTool(feature_type: self.current_state.args[0], request_subscriber: self.procRequest)
                 KeyboardPanel(request_subscriber: self.procRequest)
             }
-        } else if self.current_state == "feature tool" {
-            FeatureTool(args: self.current_args, request_subscriber: self.procRequest)
+        } else if self.current_state.state == "feature tool" {
+            FeatureTool(args: self.current_state.args, request_subscriber: self.procRequest)
         }
     }
 
     private func procRequest(request: String, args: [String]) {
         if request == "back" {
-            self.current_state = self.prev_state[0]
-//            self.current_args = self.prev_state[1 : self.prev_state.count - 1]
+            if self.prev_state.count > 0 {
+                self.current_state = self.prev_state[0]
+                self.prev_state.removeLast()
+            } else {
+                self.current_state = StateParams(state: "keys + suggestions", args: [])
+            }
         } else if request == "state" {
-            self.prev_state = [self.current_state]
-            self.prev_state += self.current_args
-
-            self.current_state = args[0]
-//            self.current_args = args[1..<args.count]
+            self.prev_state.append(self.current_state)
+            self.current_state = StateParams(state: args[0], args: args[1..<args.count])
         } else {
             print("Processing request: \(request) with args: \(args)")
         }
